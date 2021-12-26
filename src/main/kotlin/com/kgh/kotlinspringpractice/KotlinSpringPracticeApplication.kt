bce32e860a633b8ec7d93be39aca0f7c956a8b9e
gwanhyeon <gwanhyeon0525@gmail.com>
2021-12-26 22:05:44 +0900

    chore: project init

---
+++ src/main/kotlin/com/kgh/kotlinspringpractice/KotlinSpringPracticeApplication.kt
@@ -0,0 +1,11 @@
+package com.kgh.kotlinspringpractice
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class KotlinSpringPracticeApplication
+
+fun main(args: Array<String>) {
+	runApplication<KotlinSpringPracticeApplication>(*args)
+}
